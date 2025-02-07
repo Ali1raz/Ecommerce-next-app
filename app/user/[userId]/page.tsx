b0ab68e334b846e2b719b6fd8d40874fa1501007
Ali Raza <alirazakhalid1129@gmail.com>
2025-02-07 19:49:29 +0500

    update user page

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,6 +1,7 @@
 import { getUserbyId} from "@/app/actions/actions";
 import ProductsList from "@/components/ProductsList";
 import {prisma} from "@/lib/prisma";
+import {Avatar, AvatarImage} from "@/components/ui/avatar";
 
 export default async function UserPage({params}: {params: Promise<{userId: string}>}) {
     const userId = (await params).userId;
@@ -8,14 +9,28 @@ export default async function UserPage({params}: {params: Promise<{userId: strin
     const products = await prisma.product.findMany({where: {user_id : userId}});
 
     const user =await getUserbyId(userId);
-    console.log('==>', userId, user?.name);
-    products.forEach(p => console.log('--', p.user_id))
     return (
-        <div>
-            <h1>User Details</h1>
-            {products.length !== 0 ? (<ProductsList data={products}/>): (
-                <h1 className='text-center text-2xl mt-8'>No products .</h1>
-            )}
+        <div className='container w-full mx-auto'>
+            <div className='p-2 flex items-start gap-3 border'>
+                <Avatar>
+                    <AvatarImage
+                        src={user?.avatar}
+                        alt='user'
+                        className='rounded-full'
+                        width={60} height={60}
+                    ></AvatarImage>
+                </Avatar>
+                <div className='flex-1'>
+                    <h1 className='text-gray-800 text-xl font-bold tracking-wide'>{user?.name}</h1>
+                    <p>Total Products: <span className='text-gray-600'>{products.length}</span></p>
+                </div>
+            </div>
+            <div className='h-1 w-full bg-gray-600 my-3 '></div>
+            <div className='container max-w-6xl mx-auto'>
+                {products.length !== 0 ? (<ProductsList data={products}/>): (
+                    <h1 className='text-center text-2xl mt-8'>No products by the user.</h1>
+                )}
+            </div>
         </div>
     )
 }
