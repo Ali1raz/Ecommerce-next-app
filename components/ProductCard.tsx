15dcd0df7d7adfbccd6e820aa1790eafb7e9a50b
Ali Raza <alirazakhalid1129@gmail.com>
2025-02-08 16:12:01 +0500

    update: ProductCard show owner name

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,38 +3,41 @@ import Link from "next/link";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
 import {formatDate, TProductProps} from "@/utils";
+import {getUserbyId} from "@/app/actions/actions";
 
-export default function ProductCard({product}: { product: TProductProps}) {
+export default async function ProductCard({product}: { product: TProductProps}) {
     const {
         name,
         id,
+        user_id,
         price,
         rating,
         created_at,
         stock_quantity
     } = product;
 
+    const {name: ownerName} = await getUserbyId(user_id);
 
     return (
         <li className="flex flex-col  shadow-sm  bg-gray-800">
             <div className="flex items-center justify-center min-w-[157px] min-h-[180px] bg-slate-200 relative">
                 {/*<Image ...></Image>*/}
                 <Badge variant='destructive'
-                       className='absolute top-1.5 right-1.5 py-1 px-4 text-xs   '>{stock_quantity}</Badge>
+                       className='absolute top-1.5 right-1.5 py-1 px-4 text-xs'>{stock_quantity}</Badge>
             </div>
 
             <div className='p-4 text-white'>
-                <p className='line-clamp-1  capitalize '>{name}</p>
+                <Link href={`/products/${id}`} className='hover:text-gray-200'>
+                    <p className='line-clamp-1 capitalize'>{name}</p>
+                </Link>
                 <div className='flex  items-center justify-between mt-1'>
-                    <p className="text-amber-600 text-lg font-bold flex gap-1 items-center">
+                    <p className="text-amber-600 flex gap-1 items-center">
                         <span className="text-xs">Rs</span>
-                        <span>{price}</span>
+                        <span className='font-bold text-lg'>{price}</span>
                     </p>
                     <p>{rating} ⭐</p>
                 </div>
-                <Button variant='secondary' asChild className=' px-5 rounded-none py-1 mt-2'>
-                    <Link href={`/products/${id}`}>View</Link>
-                </Button>
+                <Button variant='link' className='text-gray-300 text-xs p-0 m-0' asChild><Link href={`/user/${user_id}`}>{ownerName}</Link></Button>
                 <Badge variant='secondary'
                        className='block text-white px-3 py-1 mt-4 bg-slate-700/80 text-xs w-fit'>{formatDate(created_at)}</Badge>
             </div>
