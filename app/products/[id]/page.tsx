27bf03814edce477986f18daedbaecd3846855a7
Ali Raza <alirazakhalid1129@gmail.com>
2025-05-30 21:03:39 +0500

    fix warning ProductPage component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,27 +1,29 @@
 import ProductDetails from "@/components/ProductDetails";
 import GoBackButton from "@/components/GoBackButton";
-import {get_product} from "@/app/actions/actions";
-import {notFound} from "next/navigation";
+import { get_product } from "@/app/actions/actions";
+import { notFound } from "next/navigation";
 
 type PageProps = {
-    params: {
-        id: string;
-    }
-}
+  params: {
+    id: string;
+  };
+};
 
-export default async function ProductPage({params}: PageProps) {
-    const {id} = params;
-    const product = await get_product(id);
+export default async function ProductPage({ params }: PageProps) {
+  const { id } = await params;
+  const product = await get_product(id);
 
-    if (!product) {
-        notFound()
-    }
+  if (!product) {
+    notFound();
+  }
 
-    return (
-        <div className='mt-2 max-w-4xl mx-auto'>
-            <GoBackButton label='Back'/>
-            <h1 className='text-2xl border-b text-center font-bold mb-4'>Product Details</h1>
-            <ProductDetails product={product}/>
-        </div>
-    );
+  return (
+    <div className="mt-2 max-w-4xl mx-auto">
+      <GoBackButton label="Back" />
+      <h1 className="text-2xl border-b text-center font-bold mb-4">
+        Product Details
+      </h1>
+      <ProductDetails product={product} />
+    </div>
+  );
 }
